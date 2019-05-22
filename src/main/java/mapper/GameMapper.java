f57f6d1be2d58573858baa05308ebb1c9f341951
duguanguyan <duguangyuan@seassoon.com>
2019-05-22 13:32:21 +0800

    开始,结束和落子

---
+++ src/main/java/mapper/GameMapper.java
@@ -0,0 +1,11 @@
+package mapper;
+
+import pojo.Game;
+
+public interface GameMapper {
+
+    public Integer gameStart(Game game);
+
+    public Integer gameEnd(Game game);
+
+}
---
+++ src/main/java/service/GameService.java
@@ -0,0 +1,11 @@
+package service;
+
+import pojo.Game;
+
+public interface GameService {
+
+    public Integer gameStart(Game game);
+
+    public Integer gameEnd(Game game);
+
+}
